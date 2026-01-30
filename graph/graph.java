b132f612441eb0380732d6e82b6ff6f855ff5b73
Anmol Srivastava <srivastavaanmol977@gmail.com>
2026-01-30 16:08:56 +0530

    Adjacency matrix representation of graph

---
+++ graph/graph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class graph{
+    void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of vertices");
+        int v=sc.nextInt();
+        System.out.println("Enter the no. of edges");
+        int e=sc.nextInt();
+        int adj[][]=new int[v][v];
+        for(int i=0;i<e;i++){
+            System.out.println("Enter the edge "+(i+1));
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            adj[a][b]=1;
+            adj[b][a]=1;
+        }
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                System.out.print(adj[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
